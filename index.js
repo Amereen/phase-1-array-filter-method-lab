56d8325481bbe3ae7287190d016f086b3cb22267
Amereen Hanvel <amereenhanvel786@gmail.com>
2022-09-03 14:04:45 -0400

    commit tree 7bf9f08315100721192ae24cb52823cb9536bf94

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, string){
+let array = drivers.filter(driver => driver.toUpperCase() === string.toUpperCase())
+  return array
+}
+
+function fuzzyMatch(drivers, string){
+let array = drivers.filter(driver=> driver[0]=== string[0])
+return array
+}
+
+
+function matchName(driver,string){
+let object = driver.filter(driver => driver.name === string )
+ return object
+}
